e5a4e8d75aa59e2587799f062a758eef44df46fe
Less <63160418+NLessW@users.noreply.github.com>
2023-06-19 00:11:27 +0900

    Update CardPay.java

--- sub1/CardPay.java
+++ sub1/CardPay.java
@@ -1,59 +1,22 @@
 package sub1;
 
 public class CardPay {
-    private int cardNum; // 카드번호
-    private int cardPassword; // 카드 비밀번호
-    private int maxPay; // 결제 한도
-    private int numCLC; // CLC 번호
-    private int maxDate; // 유효 기간
+	private String number;
+    private String clc;
+    private String date;
+    private String password;
+    private double balance;
 
-    // 생성자
-    public CardPay(int cardNum, int cardPassword, int maxPay, int numCLC, int maxDate) {
-        this.cardNum = cardNum;
-        this.cardPassword = cardPassword;
-        this.maxPay = maxPay;
-        this.numCLC = numCLC;
-        this.maxDate = maxDate;
+    public void Card(String number, String clc, String date, String password) {
+        this.number = number;
+        this.clc = clc;
+        this.date = date;
+        this.password = password;
+        this.balance = 0.0;
     }
 
-    // 게터와 세터 메서드
-    public int getCardNum() {
-        return cardNum;
-    }
-
-    public void setCardNum(int cardNum) {
-        this.cardNum = cardNum;
-    }
-
-    public int getCardPassword() {
-        return cardPassword;
-    }
-
-    public void setCardPassword(int cardPassword) {
-        this.cardPassword = cardPassword;
-    }
-
-    public int getMaxPay() {
-        return maxPay;
-    }
-
-    public void setMaxPay(int maxPay) {
-        this.maxPay = maxPay;
-    }
-
-    public int getNumCLC() {
-        return numCLC;
-    }
-
-    public void setNumCLC(int numCLC) {
-        this.numCLC = numCLC;
-    }
-
-    public int getMaxDate() {
-        return maxDate;
-    }
-
-    public void setMaxDate(int maxDate) {
-        this.maxDate = maxDate;
+    public void addBalance(double amount) {
+        balance += amount;
     }
 }
+
